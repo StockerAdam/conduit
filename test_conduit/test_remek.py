5c2ef2f56fed9f66a277e582ab02e7fd1b4b733c
Adam Stocker <adam.stocker@flowacademy.hu>
2022-06-26 12:24:29 +0200

    11+1 teszteset commentezése (kész)

--- test_conduit/test_remek.py
+++ test_conduit/test_remek.py
@@ -23,7 +23,9 @@ class TestConduit(object):
     def teardown(self):
         self.browser.quit()
 
-    # 1.Adatkezelési nyilatkozat használata
+
+    # 1.Adatkezelési nyilatkozat használata: Az adatkezelési nyilatkozatot elfogadjuk, majd ellenőrizzük,
+    # hogy ez megtörtént-e (eltűnt-e az erről szóló értesítés).
     def test_cookie(self):
         cookie_bar = WebDriverWait(self.browser, 7).until(
             EC.presence_of_element_located((By.XPATH, '//div[@class = "cookie__bar__buttons"]')))
@@ -46,7 +48,9 @@ class TestConduit(object):
 
         assert not len(accept_btn_list_after_click)
 
-    # 2.a Regisztráció helytelen adatokkal
+
+    # 2.a Regisztráció helytelen adatokkal: A regisztráció során minden invalid értékeket használva, ellenőrizzük,
+    # hogy megkapjuk-e a sikerelen regisztrációról szóló üzenetet.
     def test_registration_invalid(self):
         registration(self.browser, user_invalid["name"], user_invalid["email"], user_invalid["password"])
 
@@ -58,13 +62,17 @@ class TestConduit(object):
         assert result_message.text == sys_messages["invalid_reg"]
         assert result_message2.text == sys_messages["invalid_email"]
 
-    # 2.b Regisztráció megfelelő (valid) adatokkal
+
+    # 2.b Regisztráció megfelelő (valid) adatokkal: Regisztráció valid adatokkal. Ellenőrzés itt külön nincs,
+    # mert a folyamat csak egyszer végrehajtható, így ez kiegészítése a regisztrációról szóló esetnek.
     def test_registration_valid(self):
         registration(self.browser, user_valid["name"], user_valid["email"], user_valid["password"])
 
         time.sleep(2)
 
-    # 3. Bejelentkezés valid, regisztrált adatokkal
+
+    # 3. Bejelentkezés valid, regisztrált adatokkal: A 2.b pontban regisztrált adatokkal való belépés után ellenőrizzük,
+    # hogy a profil név a kezdőoldalon megegyezik-e a regisztrál névvel.
     def test_sign_in(self):
         sign_in(self.browser, user_valid["email"], user_valid["password"])
 
@@ -74,7 +82,9 @@ class TestConduit(object):
 
         assert user_profile.text == user_valid['name']
 
-    # 4. Adatok listázása
+
+    # 4. Adatok listázása: kilistázzuk az oldalon található népszerű (popular) tag-eket. Ellenőrizzük,
+    # hogy az oldalon található tagek száma megegyezik-e az általam készített lista elemszámával.
     def test_list_popular_tags(self):
         popular_tags = self.browser.find_elements_by_xpath('//a[@class="tag-pill tag-default"]')
 
@@ -83,7 +93,9 @@ class TestConduit(object):
             list_of_pop_tags.append(f'{i + 1}. popular tag: {k.text}')
         assert len(list_of_pop_tags) == len(popular_tags)
 
-    # 5. Adatok lementése felületről
+
+    # 5. Adatok lementése felületről: A népszerű (popular) tag-eket kigyűjtjük és lementjük egy listába sorszámozva.
+    # A kigyűtő ciklus lefutásainak számát összehasonlítjuk az oldalon található tagek számával.
     def test_export_data(self):
         with open("pop_tag_list.txt", "w", encoding="UTF-8", newline='') as file:
             popular_tags = self.browser.find_elements_by_xpath(
@@ -94,7 +106,9 @@ class TestConduit(object):
                 counter += 1
         assert counter == len(popular_tags)
 
-    # 6. Több oldalas lista bejárása
+
+    # 6. Több oldalas lista bejárása: Az alsó oldalnavigációs menűben lapozva ellenőrizzük,
+    # hogy az oldalszám megegyező-e.
     def test_page_navigation(self):
         TestConduit.test_sign_in(self)
         pages = self.browser.find_elements_by_xpath('//a[@class="page-link"]')
@@ -104,7 +118,9 @@ class TestConduit(object):
             current_page = self.browser.find_element_by_xpath('//li[@class="page-item active"]')
             assert page.text == current_page.text
 
-    # 7. Új adat (cikk) bevitel
+
+    # 7. Új adat (cikk) bevitel: Egy előre meghatározott tartalmú cikk (article) felvitele. Ellenőrizzük,
+    # hogy cikk fő tartalma (body) a létrehozást követően, megegyezik-e az általunk bevitt tartalommal.
     def test_adding_new_input(self):
         TestConduit.test_sign_in(self)
         create_new_article(self.browser, new_article['title'], new_article['about'], new_article['body'], new_article['tag'])
@@ -114,7 +130,9 @@ class TestConduit(object):
         article_body = self.browser.find_element_by_xpath('//p')
         assert article_body.text == new_article['body']
 
-    # 8. Ismételt és sorozatos adatbevitel adatforrásból
+
+    # 8. Ismételt és sorozatos adatbevitel adatforrásból. Egy csv kiterjesztésű file-ból több cikket felviszünk az oldalra.
+    # Minden egyes cikk felvitele után ellenőrizzük hogy cikk fő tartalma (body) a létrehozást követően, megegyezik-e az általunk bevitt tartalommal.
     def test_import_articles(self):
         TestConduit.test_sign_in(self)
         with open('test_conduit/articles.csv', 'r') as file:
@@ -125,7 +143,10 @@ class TestConduit(object):
                 article_body = self.browser.find_element_by_xpath('//p')
                 assert article_body.text == row[2]
 
-    # 9. Adat vagy adatok törlése (MÓDOSÍTJA A USERNAMET!!!!)
+
+    # 9. Adat vagy adatok törlése: Az oldalon található első cikk szerzőjének nevére megváltoztatva a profil nevünket,
+    # jogot kapunk a felhasznált szerző összes cikkjének a módosításához, törléséhez. Töröljük az első cikket.
+    # Két elleőrzést végzünk. Az egyik az, hogy a név változás megtörtént-e, a másik pedig, hogy törlődött-e a cikk (cikkszám alapján).
     def test_delete_article(self):
         TestConduit.test_sign_in(self)
         time.sleep(1)
@@ -155,14 +176,17 @@ class TestConduit(object):
 
         change_name(self.browser, "Entertester")
 
-    # 10. Meglévő adat módosítás
+
+    # 10. Meglévő adat módosítás: Megváltoztatjuk a profil nevünket, majd ellenőrizzük az erről kapott üzenetet.
     def test_name_change(self):
         TestConduit.test_sign_in(self)
         change_name(self.browser, "Entertester")
 
         assert self.browser.find_element_by_xpath('//div[@class="swal-title"]').text == 'Update successful!'
 
-    # 11. Kijelentkezés
+
+    # 11. Kijelentkezés: Kijelentkezünk, majd ellenőrizzük, hogy megjelent-e a "Sign in" (bejelentkezés) menüpont,
+    # azaz ki vagyunk-e jelentkezve.
     def test_logout(self):
         TestConduit.test_sign_in(self)
         logout_btn = self.browser.find_element_by_xpath('//a[@active-class="active"]')
